09df535e4b219018141d6edce39fa43dae15a230
Nikita Chekalinsky <NNikitos0901@gmail.com>
2021-09-20 22:43:28 +0300

    delete wpf

--- 12lab/lab12_1/lab12_1/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lab12_1
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
-}
--- 12lab/lab12_2/lab12_2/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lab12_2
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
-}
